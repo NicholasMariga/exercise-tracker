8d68881e69dec1289c9d9bfcfffcc3d38833e8c4
Nicholas Mariga <dolphnikoz@gmail.com>
2021-05-10 19:54:10 +0300

    A  simple Exercise tracker implementing MERN stack (MongoDB, Express, React, NodeJS)..Feel free to get the code and upgrade

---
+++ mern-exercise-tracker/src/App.js
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar-component";
+import ExerciseList from "./components/exercise-list-component";
+import EditExercise from "./components/edit-exercise-component";
+import CreateExercises from "./components/create-exercise-component";
+import CreateUser from "./components/create-user-component";
+
+
+function App() {
+    return (
+    <Router>
+        <div className="container">
+        <Navbar/>
+        <br/>
+        <Route path="/" exact component ={ExerciseList}/>
+        <Route path="/edit/:id" component ={EditExercise}/>
+        <Route path="/create" component ={CreateExercises}/>
+        <Route path="/user" component ={CreateUser}/>
+        </div>      
+    </Router>
+    );
+}
+
+export default App; 
